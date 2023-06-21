3e1a7e73a976da65340d2a97da63edbef03ae6fc
smitsavaliya321@gmail.com <smitsavaliya321@gmail.com>
2023-06-21 06:17:07 -0700

    skip the value in for loop using contiues

---
+++ programme1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int i;
+
+cout<<"after series:";
+
+for(i=1;i<15;i++)
+{
+if(i==6 || i==9)
+{
+continue;
+}
+
+
+cout<<i<<endl;
+
+}
+
+
+
+}
