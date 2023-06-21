7dec5319c54ee3a61de3c58e7a373f1c9660fc8b
smitsavaliya321@gmail.com <smitsavaliya321@gmail.com>
2023-06-21 05:58:29 -0700

    add input of two user

--- programme.cpp
+++ programme.cpp
@@ -2,9 +2,13 @@
 using namespace std;
 int  main()
 {
-   int x=50, y=60, *a, *b, temp;
- 
- 
+   int x, y, *a, *b, temp;
+
+cout<<"entre value x:";
+cin>>x;
+cout<<"entre value y:";
+cin>>y;
+
    a = &x;
    b = &y;
  
@@ -12,9 +16,9 @@ int  main()
    *b = *a;   //60=50
    *a = temp; // 50
  
-   cout<<"\nafter swapping is x="<<x;
+   cout<<"\nafter swapping is x:"<<x<<"\n after swapping is y:"<<y;
  
-   cout<<"\nafter swapping is y="<<y;
+//    cout<<"\nafter swapping is y="<<y;
  
    return 0;
 }
