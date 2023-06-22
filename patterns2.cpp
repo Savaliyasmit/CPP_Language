80104dfdc0ca914f21aab3c3c47827ceea5690f9
smitsavaliya321@gmail.com <smitsavaliya321@gmail.com>
2023-06-21 20:19:37 -0700

    string pattern

---
+++ patterns2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i,j;
+    char name [11]="SKILLQUODE";
+
+    for(i=0;i<=9;i++)
+{
+    for(j=0;j<=i;j++)
+    {
+    cout<<" "<<name[j];
+    }
+    cout<<"\n";
+}
+  
+
+}
