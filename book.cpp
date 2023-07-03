373f6420d472adf2b41b52fb402fe3ae7b9d8b5f
smitsavaliya321@gmail.com <smitsavaliya321@gmail.com>
2023-07-02 21:00:09 -0700

    calculate flight avarage fule

---
+++ book.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+class book
+{
+   private:
+
+   int booknum;
+   char b[20];
+   float price;
+   total_cost()
+   {
+    
+   }
+
+
+
+};
---
+++ tast.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Test
+{
+private:
+    int testcode,NoCandidate,CenterReqd; 
+    string Description; //  getline(cin ,Description); for tak a input string 
+
+    float Calcntr()
+    {
+        return (NoCandidate/100+1);
+    }
+    
+public:
+
+  void Schedule()
+   {
+     cout<<"Enter a Test Code : ";  //  230
+     cin>>testcode;  
+     cout<<endl<<"Enter a Description : "; //  National Level Exam // Sem-V
+     cin>>Description;
+     cout<<endl<<"Enter a NoCandidate : ";  // i am Enter my set No 14611 
+     cin>>NoCandidate;
+     Disp_test();
+    }
+
+   void Disp_test()
+    {
+     cout<<"Tast Code : "<<testcode;
+     cout<<endl<<"Description : "<<Description;
+     cout<<endl<<"NoCandidate : "<<NoCandidate;
+     CenterReqd = Calcntr();
+     cout<<endl<<"Your CenterReqd is : "<<CenterReqd;
+    }
+    
+};
+
+
+int main ()
+{
+    Test exam;
+
+    exam.Schedule();
+}
