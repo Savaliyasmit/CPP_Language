6abd4bfdfbd061ccac8d187e0a575dea9c9ec40a
smitsavaliya321@gmail.com <smitsavaliya321@gmail.com>
2023-07-05 22:00:05 -0700

    some cpp language function

---
+++ cppfunction.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<math.h>
+#include<stdlib.h>
+using namespace std;
+
+int main()
+{
+    cout<<"ans:"<<rand()%10<<endl;
+    cout<<"ans:"<<pow(2,6)<<endl;
+    cout<<"ans:"<<sqrt(16)<<endl;
+    cout<<"ans:"<<sqrt(8)<<endl;
+    cout<<"ans:"<<abs(14.2)<<endl;
+    cout<<"ans:"<<abs(-13.0)<<endl;
+    cout<<"ans:"<<round(15.2)<<endl;
+    cout<<"ans:"<<round(-8.9)<<endl;
+    cout<<"ans:"<<floor(4.8)<<endl;
+    cout<<"ans:"<<floor(-8.9)<<endl;
+    cout<<"ans:"<<ceil(7.8)<<endl;
+    cout<<"ans:"<<ceil(-6.7)<<endl;
+}
+    
